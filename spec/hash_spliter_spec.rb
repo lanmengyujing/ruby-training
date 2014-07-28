b7d5126651745cf9efc6b0f0aeb573f74856d4d5
Liu Jing <liujing92684@163.com>
2014-07-29 01:45:54 +1000

    handle exception correctly

--- spec/hash_spliter_spec.rb
+++ spec/hash_spliter_spec.rb
@@ -10,4 +10,17 @@ describe HashSpliter do
     expect(results).to eql([{:a => 1, :b => 2}, {:c => 3, :d => 4}, {:e => 5, :f => 6}])
   end
 
+  it 'should return two array after given split key' do
+    hash = {'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5, 'f' => 6}
+    results = subject.split_hash_by_key(hash, 'd')
+
+    expect(results).to eql([{"a" => 1, "b" => 2, "c" => 3}, {"d" => 4, "e" => 5, "f" => 6}])
+  end
+
+  it 'should return raise exception' do
+    hash = {:a => 1, :b => 2, :c => 3, :d => 4, :e => 5, :f => 6}
+
+    expect { subject.split_hash_by_key(hash, 'n') }.to raise_error
+  end
+
 end
